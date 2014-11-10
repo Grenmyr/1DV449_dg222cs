e2ee8e47fc49d41ba7183525f5c5a51326bd0046
dav <dg222cs@student.lnu.se>
2014-11-10 13:11:13 +0100

    Kan nu skrapa med omskrapningstid 20 sekunder.

--- routes/scrape.js
+++ routes/scrape.js
@@ -5,18 +5,36 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
+var fs = require('fs');
+
 
 /* GET users listing. */
 router.get('/', function(req, res) {
+    var url = 'http://coursepress.lnu.se/kurser';
+    var date = new Date().getTime();
+    fs.readFile('scrapeResult.json',function(err,data){
+      var parse = JSON.parse(data);
+        if(parse.lastScrapeTime < date - 20000){
+            console.log("scrapade om");
+            scrape(url);
+        }
+        else{
+            console.log("scrapade INT om");
+        }
+    });
+        //300000
+        //kolla om fil finns och dess datum är mindre än 5 minuter sedan
 
-    var url='http://coursepress.lnu.se/kurser';
 
 
-    scrape(url);
+    //scrape(url);
 
     res.send();
 });
 var Json = {};
+
+
+
 function scrape (url){
 
 
@@ -55,28 +73,32 @@ function scrape (url){
                             }
 
                         });
+
                         var courseDescription = $('.entry-content').text();
                          courseDescription = checkInformation(courseDescription);
-
                         Json[courseName].courseDescription = courseDescription;
 
-
+                        // lastPost header
                         var lastPostHeader = $('.entry-header .entry-title').first().text();
-                        Json[courseName].latestPost={};
                         lastPostHeader = checkInformation(lastPostHeader);
-                        Json[courseName].latestPost = {headerTitle : lastPostHeader};
 
+                        // lastPost author
                         var author = $('.entry-header .entry-byline strong').first().text();
                         author = checkInformation(author);
-                        Json[courseName].latestPost = {author : author };
 
+                        // lastPost byline/text
                         var date = $('.entry-header .entry-byline').first().text();
-
                         var match = date.match(/(\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2})/);
-                        if(match !== null) {
-                            Json[courseName].latestPost = {date : match[0] };
+                        if(match === null) {
+                            match = checkInformation(match);
                         }
-                  
+                        // Populate my Coursname object with 3 lastPost data above.
+                        Json[courseName].latestPost = {
+                            date : match[0],
+                            headerTitle : lastPostHeader,
+                            author : author
+                        };
+                        //console.log(Json.lastScrapeTime);
                     }
                 });
             }
@@ -90,18 +112,26 @@ function scrape (url){
 }
 
 function scrapeOn (url){
-    //console.log(url);
+
     var homeURL = 'http://coursepress.lnu.se';
 
     if (url !== undefined) {
         url = homeURL + url;
-        //console.log(url);
         scrape(url);
+    }
+    else{
+        Json.lastScrapeTime = new Date().getTime();
+        fs.writeFile('scrapeResult.json', JSON.stringify(Json, null,4),function(err){
+            if(err){
+                console.log("whoops fel vid sparande av Json fil.");
+            }
+            console.log('Success save file to disc');
+        })
     }
 }
 
 function checkInformation (string){
-    if (string === '') { return string = 'no information'; }
+    if (string === '' | string === null) { return string = 'no information'; }
     return string;
 }
 
