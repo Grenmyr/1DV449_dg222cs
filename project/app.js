dd395c98eb8e5bb5df0167d3e845e81c78cc4a85
dav <dg222cs@student.lnu.se>
2015-01-03 12:39:30 +0100

    lagt in favicon

--- project/app.js
+++ project/app.js
@@ -4,17 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var jade = require('jade');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var fs = require('fs');
 var app = express();
 
-// code assorted with fetching and storing data from eniro.
+// request used for url requests from eniro.
 var request = require('request');
-// end
 
 // CODE ASSORTED WITH MONGODB
 // New Code
@@ -27,8 +24,7 @@ var db = monk('localhost:27017/enirodb');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -164,16 +160,12 @@ function prepareData(data) {
     return parse;
 }
 
-
-
 // Functions handling CRD with mongodb using Monk.
 function insert(search, data) {
     dropCollection(search);
-
     var collection = db.get(search.geo_area + search.search_word);
     collection.insert(data, function (err) {
         if (err) {
-            // If it failed, return error
             console.log("error inserting");
         }
         else {
@@ -184,6 +176,9 @@ function insert(search, data) {
 function find(search, callback) {
     var collection = db.get(search.geo_area + search.search_word);
     collection.find( {}, function (err, data) {
+        if (err) {
+            console.log("error when using Find");
+        }
         callback(data);
     });
 }
